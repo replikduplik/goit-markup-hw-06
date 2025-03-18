02f64869a364ff9c5567ff89196166345306c062
Çağdaş Şahin <3cagdas3@gmail.com>
2025-03-18 14:48:20 +0300

    pop-up-order işlevselliği

--- js/main.js
+++ js/main.js
@@ -1,28 +1,19 @@
-// Eklenen JavaScript
-document.addEventListener("DOMContentLoaded", function () {
-  // Mobil menü işlevselliği
-  const burgerButton = document.getElementById("burgerButton");
-  const closeMenuButton = document.getElementById("closeMenuButton");
-  const mobileMenu = document.getElementById("mobileMenu");
+// Pop-up işlevselliği
+document.addEventListener('DOMContentLoaded', function () {
+	const popup = document.querySelector('.pop-up-container');
+	const openPopupBtn = document.querySelector('.hero-button');
+	const closePopupBtn = document.querySelector('.pop-up-close-button');
 
-  burgerButton.addEventListener("click", function () {
-    mobileMenu.classList.add("is-open");
-  });
-
-  closeMenuButton.addEventListener("click", function () {
-    mobileMenu.classList.remove("is-open");
-  });
-
-  // Modal işlevselliği
-  const orderServiceButton = document.querySelector(".solutions-button");
-  const closeModalButton = document.getElementById("closeModal");
-  const modalOverlay = document.getElementById("modalOverlay");
-
-  orderServiceButton.addEventListener("click", function () {
-    modalOverlay.classList.add("is-open");
-  });
-
-  closeModalButton.addEventListener("click", function () {
-    modalOverlay.classList.remove("is-open");
-  });
+	popup.style.display = 'none';
+	openPopupBtn.addEventListener('click', function () {
+		popup.style.display = 'block';
+	});
+	closePopupBtn.addEventListener('click', function () {
+		popup.style.display = 'none';
+	});
+	window.addEventListener('click', function (event) {
+		if (event.target === popup) {
+			popup.style.display = 'none';
+		}
+	});
 });
